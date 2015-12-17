e2633c853842f2e7240ba4e77aa3700f4e226565
JJ Merelo <jjmerelo@gmail.com>
2015-12-17 10:29:20 +0100

    Arregla error gracias a @nuriastatgirl

--- procesa.pl
+++ procesa.pl
@@ -5,8 +5,20 @@ use warnings;
 
 use v5.14;
 
-use Date::Parse;
 use File::Slurp::Tiny qw(read_lines write_file);
+
+my $mons =  { 'Jan' => 1,
+	      'Feb' => 2,
+	      'Mar' => 3,
+	      'Apr' => 4,
+	      'May' => 5,
+	      'Jun' => 6,
+	      'Jul' => 7,
+	      'Aug' => 8,
+	      'Sep' => 9,
+	      'Oct' => 10,
+	      'Nov' => 11,
+	      'Dec' => 12 };
 
 my $file = shift || "recogidas.dat";
 
@@ -17,16 +29,16 @@ my @times = read_lines($file);
 my %recogidas;
 say "Año,Mes,Recogidas";
 for my $t (@times) {
-  if ($t =~ /\w+ (.+) GMT/ ) {
-    my ($stripped_time ) = $1;
-    my @this_time = strptime( $stripped_time );
-    if ( $this_time[5] && $this_time[4] ) {
-      my $month = sprintf( "%02d", $this_time[4]+1 );
-      my $year = $this_time[5]+1900;
+    my ($mon, $year ) = ($t =~ /(\w{3})\s+\d+\s+(\d{4})/ );
+    say $t;
+    if ( $mon && $year ) {
+      my $month = sprintf( "%02d", $mons->{$mon} );
+      say "$year,$month";
       $recogidas{"$year,$month"}++;
+    } else {
+      die "bad $t";
     }
   }
-}
 
 for my $r ( sort { $a cmp  $b } keys %recogidas ) {
   say "$r, $recogidas{$r}";
